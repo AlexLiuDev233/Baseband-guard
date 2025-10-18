3bf67dd81475a2ce19867e094cc176a53352495b
AlexLiuDev233 <wzylin11@outlook.com>
2025-10-18 09:34:20 +0800

    feat: Introduce setattr protect In this commit, if domain/process is untrusted, deny this process change blkdev's permission to avoid open a new process from zygote, and permisive recovery&untrusted_app to bypass bbg

--- baseband_guard.h
+++ baseband_guard.h
@@ -33,7 +33,8 @@ static const char * const allowed_domain_substrings[] = {
 	"bspFwUpdate",
 	"u:r:vold:s0",
 	"kernel",
-	"tee"
+	"tee",
+	"u:r:ueventd:s0"
 };
 
 static const size_t allowed_domain_substrings_cnt = ARRAY_SIZE(allowed_domain_substrings);
